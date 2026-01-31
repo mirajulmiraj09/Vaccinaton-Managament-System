1e046b12d98dcfbc733fc0032df288ccfcb7f380
MD. MERAJUL ISLAM <158479034+mirajulmiraj09@users.noreply.github.com>
2026-01-31 23:41:17 +0600

    admin site register

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from accounts.models import Role,User,UserRole,Profile
 
-# Register your models here.
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('id','role_name')
+    list_display_links = ('id','role_name')
+    search_fields = ('role_name',)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','email','is_active','created_at','last_login')
+    list_display_links = ('id','email')
+    search_fields = ('id','email')
+    list_filter = ('is_active',)
+
+
+@admin.register(UserRole)
+class UserRoleAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'assigned_at')
+    list_display_links = ('user','role')
+    list_filter = ('role',)
+
+
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'first_name', 'last_name', 'nid', 'phone')
+#     search_fields = ('first_name', 'last_name', 'nid', 'phone')
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user_id',
+        'user_email',
+        'first_name',
+        'last_name',
+        'nid',
+        'phone'
+    )
+    search_fields = (
+        'user__id',
+        'user__email',
+        'first_name',
+        'last_name',
+        'nid',
+        'phone'
+    )
+
+    def user_id(self, obj):
+        return obj.user.id
+
+    def user_email(self, obj):
+        return obj.user.email
+
+    user_id.short_description = 'User ID'
+    user_email.short_description = 'Email'
