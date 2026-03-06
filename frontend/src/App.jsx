e40a42d70654ad7e665fbd4a6f87b98703a72fef
JimIIT <akidul201103@gmail.com>
2026-03-06 11:51:25 +0600

    done with all setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+
 export default function App() {
   return (
-    <div className="fext items-center justify-center h-screen big blue-500">
-      <h1 className="text white text-5xl font-bold">
-        Tailwind CSS is working!
-      </h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<div>Landing Page</div>} />
+      <Route path="/login" element={<div>Login Page</div>} />
+      <Route path="/register" element={<div>Register Page</div>} />
+      <Route path="/patient/dashboard" element={<div>Patient Dashboard</div>} />
+      <Route path="/doctor/dashboard" element={<div>Doctor Dashboard</div>} />
+      <Route path="/admin/dashboard" element={<div>Admin Dashboard</div>} />
+    </Routes>
   )
 }
