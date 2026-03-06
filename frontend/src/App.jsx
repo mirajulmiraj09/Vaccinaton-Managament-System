afe5313a7ca4fe5f683ca1326ebe3049e9c527dd
JimIIT <akidul201103@gmail.com>
2026-03-06 09:59:35 +0600

    Frontend setup with react, tailwindcss adn vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="fext items-center justify-center h-screen big blue-500">
+      <h1 className="text white text-5xl font-bold">
+        Tailwind CSS is working!
+      </h1>
+    </div>
+  )
+}
